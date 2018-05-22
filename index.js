5f2d9a71dfa8ddb8a618663b3a3f8064e0441e47
futurist <26634873@qq.com>
2018-05-22 17:14:52 +0800

    first

---
+++ index.js
@@ -0,0 +1,59 @@
+var white = ' \f\n\r\t\v\u00A0\u2028\u2029'.split('')
+var NODE_WHITE_SPACE = 1
+var NODE_LINE_COMMENT = 2
+var NODE_BLOCK_COMMENT = 3
+
+function parse(str) {
+  var i=0, c, next
+  var length = str.length
+  var ast = [], node={}
+  main_loop:
+  for(;i<length;){
+    c = str[i]
+    next = str[i+1]
+    switch (node.type){
+      case NODE_WHITE_SPACE:{
+        if(white.indexOf(c) < 0) {
+             node.end = i-1
+             ast.push(node)
+             node={}
+         } else {
+           i++
+         }
+         break
+      }
+      case NODE_LINE_COMMENT:{
+        if(c==='\n') {
+            node.end = i
+            ast.push(node)
+            node={}
+        }
+        i++
+        break
+      }
+      case NODE_BLOCK_COMMENT:{
+        if(c==='*' && next==='/'){
+          i++
+          node.end = i
+          ast.push(node)
+          node={}
+        }
+        i++
+        break
+      }
+      default:{
+        if(white.indexOf(c) > -1) node = {type: NODE_WHITE_SPACE, start: i, end: i}
+        else if(c==='/' && next==='/') node = {type: NODE_LINE_COMMENT, start: i, end: i}
+        else if(c==='/' && next==='*') node = {type: NODE_BLOCK_COMMENT, start: i, end: i}
+        else break main_loop
+        i++
+      }
+    }
+  }
+  if(i>=length && node.type!=null) {
+    node.end = i-1
+    ast.push(node)
+  }
+  return ast
+}
+
